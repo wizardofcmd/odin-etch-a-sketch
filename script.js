42abeb42804753285611ce2ff0b88c5dd36d7359
Sorin <sorinnechiforora@gmail.com>
2022-12-03 18:42:01 +0000

    Add elements for options menu and sketch area

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
-// JavaScript file to create grids and handling sketching
+const sketchBox = document.querySelector(".sketch-box");
+const sketchBoxHeight = sketchBox.clientWidth;
+
+function createNewSquares(numOfSquares) {
+    let squareSize = sketchBoxHeight / numOfSquares;
+
+    for (let i = 0; i < numOfSquares; i++) {
+        row = document.createElement("div");
+        row.classList.add("row");
+        sketchBox.appendChild(row);
+
+        for (let j = 0; j < numOfSquares; j++) {
+            square = document.createElement("div");
+            square.classList.add("canvas-square");
+            square.setAttribute(
+                'style', `width: ${squareSize}px; height: ${squareSize}px`);
+            row.appendChild(square);
+        }
+    }
+}
+
+function setSquares() {
+    let userChoice = prompt("Set the number of squares per side. (Max is 100!)")
+    try {
+        if (isNaN(userChoice)) throw "not a number.";
+        if (userChoice % 1 != 0) throw "a decimal."
+        if (userChoice < 1) throw "too low.";
+        if (userChoice > 100) throw "too high.";
+
+        userChoice = parseInt(userChoice);
+        tearDownSquares();
+        createNewSquares(userChoice);
+        
+    }
+    catch (err) {
+        alert("Input is " + err);
+    }
+}
+
+function tearDownSquares() {
+    document.querySelectorAll('.row').forEach(e => e.remove());
+}
+
+createNewSquares(16);
